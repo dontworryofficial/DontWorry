1375362b97e13db4a6dd1091810c6efa92bb340c
아영 <vivid199483@gmail.com>
2019-03-15 17:51:18 +0900

    목표 지출금액 alert

--- DontWorry/ViewController/MainViewController.swift
+++ DontWorry/ViewController/MainViewController.swift
@@ -13,11 +13,22 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var pieChartView: PieChartView!
     
+    @IBOutlet weak var inMyPocketView: UIView!
+    @IBOutlet weak var inMyPocket: UILabel!
+    
+    @IBOutlet weak var spendingLimitView: UIView!
+    @IBOutlet weak var spendingLimit: UIButton!
+    
+    let numberFormatter = NumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         pieChartLoad()
         
+        spendingLimit.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        spendingLimit.titleLabel?.textAlignment = NSTextAlignment.center
+        spendingLimit.setTitle("터치하여 \n목표지출 금액을 \n설정하세요", for: .normal)
     }
     
     func pieChartLoad() {
@@ -48,6 +59,48 @@ class MainViewController: UIViewController {
         chartDataSet.colors = colors
         
         pieChartView.data = chartData
+    }
+    
+    
+    
+    // MARK: - Action
+    @IBAction func settingGoal(_ sender: UIButton) {
+        let inputAlert = UIAlertController(title: "목표지출 금액을 입력하세요.", message: nil, preferredStyle:.alert)
+        
+        let ok = UIAlertAction(title: "확인", style:.default) { (ok) in
+            //self.budgetLabel.text = inputAlert.textFields?[0].text
+            var resultBudget : String = (inputAlert.textFields?[0].text)!
+            
+            self.numberFormatter.numberStyle = .decimal
+            self.numberFormatter.maximumFractionDigits = 0 //소수점 몇자리까지 허용하는지
+            
+            resultBudget = resultBudget.replacingOccurrences(of: ",", with: "", options: .literal, range: nil)
+            
+            resultBudget = self.numberFormatter.string(from: NSNumber(value: Double(resultBudget)!))!
+            
+            self.inMyPocket.text = resultBudget
+            self.spendingLimitView.isHidden = true
+            self.inMyPocketView.isHidden = false
+            // self.goalSpend.setTitle("\(resultBudget)", for: .normal)
+            
+        }
+        
+        let cancle = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+        
+        inputAlert.addAction(cancle)
+        inputAlert.addAction(ok)
+        
+        inputAlert.addTextField { (textField) in
+            var replace = self.inMyPocket.text
+            print(replace!)
+            replace = replace?.replacingOccurrences(of: ",", with: "", options: .literal, range: nil)
+            print(replace!)
+            textField.text = replace
+            textField.placeholder = "0원"
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        self.present(inputAlert,animated: false,completion: nil)
     }
     
 }
