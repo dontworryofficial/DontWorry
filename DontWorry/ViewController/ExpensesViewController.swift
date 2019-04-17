c74c2a5456a47e657593c939edafd0aeb6b85373
아영 <vivid199483@gmail.com>
2019-04-17 18:54:06 +0900

    0417 지출화면 datePicker 진행중

--- DontWorry/ViewController/ExpensesViewController.swift
+++ DontWorry/ViewController/ExpensesViewController.swift
@@ -5,29 +5,63 @@ class ExpensesViewController: UIViewController {
     
     @IBOutlet weak var categoryCollectionView: UICollectionView!
     @IBOutlet weak var paymentBtn: UIButton!
+    @IBOutlet weak var dateTextField: UITextField!
     
-
+    private var datePicker : UIDatePicker?
+    let date = Date()
+    let dateformatter = DateFormatter()
+    
     override func viewDidLoad() {
         
         self.categoryCollectionView.dataSource = self
         super.viewDidLoad()
-
+        
+        self.dateformatter.dateFormat = "yyyy년 MM월 dd일 a H:mm"
+        let stringDate = dateformatter.string(from: date)
+        dateTextField.text = stringDate
+        
+        dateSet()
+        
+        
     }
+    
+    func dateSet(){
+        
+        dateTextField.borderStyle = .none
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .dateAndTime
+
+        view.endEditing(true)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ExpensesViewController.viewtapped))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        dateTextField.inputView = datePicker
+        
+        dateTextField.text = dateformatter.string(from: datePicker!.date)
+    }
+    
+    @objc func viewtapped(gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+
 
     @IBAction func paymentChanged(_ sender: UIButton) {
         
         let alert = UIAlertController(title: "결제방식", message: nil, preferredStyle: .actionSheet)
         
         let creditCard = UIAlertAction(title: "신용카드", style: .default) { (UIAlertAction) in
-            self.paymentBtn.titleLabel?.text = "신용카드"
+            self.paymentBtn.setTitle("신용카드", for: UIControl.State.normal)
         }
         
         let checkCard = UIAlertAction(title: "체크카드", style: .default) { (UIAlertAction) in
-            self.paymentBtn.titleLabel?.text = "체크카드"
+            self.paymentBtn.setTitle("체크카드", for: UIControl.State.normal)
         }
         
         let cash = UIAlertAction(title: "현금", style: .default) { (UIAlertAction) in
-            self.paymentBtn.titleLabel?.text = "현금"
+            self.paymentBtn.setTitle("현금", for: UIControl.State.normal)
         }
         
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
@@ -39,8 +73,9 @@ class ExpensesViewController: UIViewController {
         
         present(alert, animated: false, completion: nil)
         
-        
     }
+    
+    
 
 }
 
